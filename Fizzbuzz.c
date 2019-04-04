4c14796612b16ad7a8717aee2011bd969b683224
mki-myk-63 <44719281+mki-myk-63@users.noreply.github.com>
2019-04-04 15:24:15 +0900

    Update Fizzbuzz.c
    リファクタリング

--- Fizzbuzz.c
+++ Fizzbuzz.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
-void print_fizz() {
-    printf("Fizz");
-}
-void print_buzz() {
-    printf("Buzz");
-}
 void main() {
     for(int i = 1; i <= 100; i++) {
-        if(i % 3 == 0) { print_fizz(); }
-        if(i % 5 == 0) { print_buzz(); }
+        if(i % 3 == 0) { printf("Fizz"); }
+        if(i % 5 == 0) { printf("Buzz"); }
 
         if(i % 3 != 0 && i % 5 != 0) {
             printf("%d", i);

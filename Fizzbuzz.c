75cc0ccb5a9d22c48a5f31e742afac92869791f2
mki-myk-63 <44719281+mki-myk-63@users.noreply.github.com>
2019-04-04 15:06:36 +0900

    FizzBuzz問題

---
+++ Fizzbuzz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void fizz() {
+    printf("Fizz");
+}
+void buzz() {
+    printf("Buzz");
+}
+void main() {
+    for(int i = 1; i <= 100; i++) {
+        if(i % 3 == 0) {
+            fizz();
+        }
+        if(i % 5 == 0) {
+            buzz();
+        }
+        if (i % 3 != 0 && i % 5 != 0) {
+            printf("%d", i);
+        }
+        printf("\n");
+    }
+}

2ed5ccdcf4f040cc2fc0de0b91d7748c9143f157
mki-myk-63 <44719281+mki-myk-63@users.noreply.github.com>
2019-04-04 15:12:22 +0900

    Update Fizzbuzz.c

--- Fizzbuzz.c
+++ Fizzbuzz.c
@@ -7,12 +7,8 @@ void buzz() {
 }
 void main() {
     for(int i = 1; i <= 100; i++) {
-        if(i % 3 == 0) {
-            fizz();
-        }
-        if(i % 5 == 0) {
-            buzz();
-        }
+        if(i % 3 == 0) { fizz(); }
+        if(i % 5 == 0) { buzz(); }
         if (i % 3 != 0 && i % 5 != 0) {
             printf("%d", i);
         }

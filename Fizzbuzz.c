37514133ab25ea9fff455f811fa421cada970b12
mki-myk-63 <44719281+mki-myk-63@users.noreply.github.com>
2019-04-04 15:14:50 +0900

    Update Fizzbuzz.c
    メソッド名変更

--- Fizzbuzz.c
+++ Fizzbuzz.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-void fizz() {
+void print_fizz() {
     printf("Fizz");
 }
-void buzz() {
+void print_buzz() {
     printf("Buzz");
 }
 void main() {
     for(int i = 1; i <= 100; i++) {
-        if(i % 3 == 0) { fizz(); }
-        if(i % 5 == 0) { buzz(); }
+        if(i % 3 == 0) { print_fizz(); }
+        if(i % 5 == 0) { print_buzz(); } 
         if (i % 3 != 0 && i % 5 != 0) {
             printf("%d", i);
         }
